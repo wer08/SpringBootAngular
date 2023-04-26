4163fcda72a7d64c2ad49166e967e4f729a253ed
Wojtek <wz08061992@gmail.com>
2023-04-26 13:09:54 +0200

    added saving the report and printing functionality

--- serverapp/src/app/app.component.ts
+++ serverapp/src/app/app.component.ts
@@ -173,4 +173,24 @@ export class AppComponent implements OnInit{
       })
     );
   }
+
+  saveReport(event: Event): void{
+    switch((event.target as HTMLSelectElement).value){
+      case "pdf":
+        window.print();
+        break;
+      case "xml":
+        let dataType = 'application/vnd.ms-excel.sheet.macroEnabled.12';
+        let tableSelect = document.getElementById("servers");
+        let tableHtml = tableSelect.outerHTML.replace(/ /g,'%20');
+        let downloadLink = document.createElement('a');
+        document.body.appendChild(downloadLink);
+        downloadLink.href = 'data:' + dataType + ", " + tableHtml;
+        downloadLink.download = 'server-report.xls';
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+        break;
+    }
+
+  }
 }
